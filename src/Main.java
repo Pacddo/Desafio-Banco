4474ed2c6fd7d628b0056ceb60e6e66857dd746c
pacddo <douglas.pacheco@hotmail.com>
2022-03-07 04:11:37 -0300

    Atualização programa

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,37 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-     Cliente douglas = new Cliente();
-     douglas.setNome("Douglas");
-     Conta cc = new ContaCorrente (douglas);
-     Conta pupanca = new Contapupanca(douglas);
+        Scanner scan = new Scanner(System.in);
 
-        cc.imprimirExtrato();
-        pupanca.imprimirExtrato();
+        String Id,Abrir;
 
+        while (true) {
+            System.out.println("Deseja abrir uma conta? s/n ");
+            Abrir = scan.next();
+            if (Abrir.equals("n")) {break;
+            }else if (Abrir.equals("s")) {
+
+
+                System.out.println("Digite seu nome: ");
+                Id = scan.next();
+
+
+                Cliente pessoa = new Cliente();
+                pessoa.setNome(Id);
+                Conta cc = new ContaCorrente(pessoa);
+                Conta pupanca = new Contapupanca(pessoa);
+
+                cc.imprimirExtrato();
+                pupanca.imprimirExtrato();
+            }else {
+                System.out.println("insira um caractere correto!");
+            }
+
+
+
+
+
+        }
     }
 }
